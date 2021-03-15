49e1aad5cd264849a67b50bb83e8966d813eb082
Mike Stop Continues <mikestopcontinues@gmail.com>
2021-03-15 13:58:29 +0000

    Only router for now

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 // export
 
 export * from './router';
-export * from './ware/authWare';
-export * from './ware/corsWare';
-export * from './ware/devWare';
-export * from './ware/helmetWare';
-export * from './ware/jwtWare';
-export * from './ware/githubHookWare';
-export * from './ware/previewWare';
-export * from './ware/recaptchaWare';
+// export * from './ware/authWare';
+// export * from './ware/corsWare';
+// export * from './ware/devWare';
+// export * from './ware/helmetWare';
+// export * from './ware/jwtWare';
+// export * from './ware/githubHookWare';
+// export * from './ware/previewWare';
+// export * from './ware/recaptchaWare';
 export * from './ware/types';
